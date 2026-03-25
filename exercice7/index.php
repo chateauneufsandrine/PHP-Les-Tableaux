cc28cb7248c4eec79394f1237b5f1b09add4c793
Châteauneufsandrine <chateauneufsandrine188@gmail.com>
2026-03-25 11:40:57 +0100

    add exercice7

--- exercice7/index.php
+++ exercice7/index.php
@@ -1,6 +1,17 @@
 <?php 
 // EXERCICE7///////////////////////////////////////////////////////////
-// Avec le tableau de l'exercice 5, ajouter la ligne correspondant au département de la ville de Reims
+// Avec le tableau de l'exercice 5, ajouter la ligne correspondant 
+// au département de la ville de Reims
+
+$departement = [
+   02 => "Aisne",
+   51 => "Marne",  
+   59 => "Nord", 
+   60 => "Oise",
+   62 => "Pas-de-Calais", 
+   80 => "Somme"
+   ];
+
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +22,8 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php foreach ($departement  as $numero => $nom) { ?>
+<h2><?= $numero ?> - <?= $nom ?></h2>
+<?php } ?>
 </body>
 </html>
