2cad63e9a465a5d8aa4b90affe9d52bb98a432e8
Châteauneufsandrine <chateauneufsandrine188@gmail.com>
2026-03-25 11:38:14 +0100

    add exercice6

--- exercice6/index.php
+++ exercice6/index.php
@@ -1,6 +1,15 @@
 <?php 
 // EXERCICE6////////////////////////////////////////////////////
 // Avec le tableau de l'exercice 5, afficher la valeur de l'index 59
+
+$departement = [
+   02 => "Aisne",  
+   59 => "Nord", 
+   60 => "Oise",
+   62 => "Pas-de-Calais", 
+   80 => "Somme"
+   ];
+
 ?>
 
 
@@ -12,6 +21,6 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h2><?= $departement [59] ?></h2>
 </body>
 </html>
