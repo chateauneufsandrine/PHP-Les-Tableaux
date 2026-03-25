4e81ee44bbf931bbaae8cb2a4066efe7097b7983
Châteauneufsandrine <chateauneufsandrine188@gmail.com>
2026-03-25 11:13:27 +0100

    exercice2

--- exercice2/index.php
+++ exercice2/index.php
@@ -1,6 +1,10 @@
 <?php 
 // EXERCICE2/////////////////////////////////////////////////////////////////////////////////
 // Avec le tableau de l'exercice 1, afficher la valeur de la troisième ligne de ce tableau.
+$tableauMonths = ["janvier","février","mars","avril","mai","juin","juillet",
+"aout","septembre","octobre","novembre","décembre"];
+
+
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +15,6 @@
     <title>Document</title>
 </head>
 <body>
-    
+   <h2><?= $tableauMonths [2] ?></h2>
 </body>
 </html>
